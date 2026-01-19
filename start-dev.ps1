276fabcfb7c876232356264e1cbbb00da0e10fb6
yuanli <frank.yuanli@gmail.com>
2026-01-20 00:04:55 +0800

    删除不必要的文件

--- start-dev.ps1
+++
@@ -1,3 +0,0 @@
-$env:Path = "C:\nodejs;" + [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-cd $PSScriptRoot
-npm run tauri dev
